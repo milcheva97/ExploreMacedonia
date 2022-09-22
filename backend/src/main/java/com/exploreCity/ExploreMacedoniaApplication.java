787c7e781b6cd6aa3c70ae82f7511537e8968a52
Tanja Milcheva Ivanova <milchevatanja@gmail.com>
2022-09-22 15:49:21 +0200

    Starting with app development

---
+++ backend/src/main/java/com/exploreCity/ExploreMacedoniaApplication.java
@@ -0,0 +1,11 @@
+package main.java.com.exploreCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreMacedoniaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreMacedoniaApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/exploreCity/assets/JenaAssets.java
@@ -0,0 +1,5 @@
+package main.java.com.exploreCity.assets;
+
+public class JenaAssets {
+    public static String SPARQLEndpoint = "https://dbpedia.org/sparql";
+}
